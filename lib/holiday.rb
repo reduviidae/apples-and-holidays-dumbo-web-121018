ef60fdde960b4f1b1c19b1f61e5676179aa758ce
Wil Cook <wil.cook.is@gmail.com>
2018-11-26 16:17:14 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,26 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  # binding.pry
+  holiday_hash[:winter].map do |key, value|
+    value << supply
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply  
 end
+
+# merge use for hashes from ruby-doc 
+# h1 = { "a" => 100, "b" => 200 }
+# h2 = { "b" => 254, "c" => 300 }
+# h1.merge(h2)   #=> {"a"=>100, "b"=>254, "c"=>300}
+# h1.merge(h2){|key, oldval, newval| newval - oldval}
+#               #=> {"a"=>100, "b"=>54,  "c"=>300}
+# h1             #=> {"a"=>100, "b"=>200}
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  new_holiday = {holiday_name => supply_array}
+  holiday_hash[season].merge(new_holiday)
+  # binding.pry
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
